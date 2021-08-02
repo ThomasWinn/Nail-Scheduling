55e725c62ae610e994b807fe76649a4bca0a3b57
Thomas Nguyen and Tin Tran <tpnguyen1999@gmail.com>
2021-08-01 21:14:56 -0500

    First Commit

---
+++ NailScheduling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NailScheduling
+//
+//  Created by Thomas Nguyen on 8/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NailScheduling/NailSchedulingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NailSchedulingApp.swift
+//  NailScheduling
+//
+//  Created by Thomas Nguyen on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NailSchedulingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
